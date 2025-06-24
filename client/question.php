07b56d56903bf5691c1a3fec72bcc28059a6972c
Tushar <tusharpatel1093@gmail.com>
2025-06-24 19:28:33 +0530

    Create Question page and Questing Listing functionality

---
+++ client/question.php
@@ -0,0 +1,16 @@
+<div class="container">
+    <h1 class="heading">Questions</h1>
+    <div class="col-8">
+        <?php
+        include("/Php/www/Discuss_Project/common/db.php");
+        $query = "select * from questions";
+        $result = $conn->query($query);
+        foreach ($result as $row) {
+            $title = $row['title'];
+            echo "<div class='row questionList'>
+        <h4><a href='#'>$title</a></h4>
+        </div>";
+        }
+        ?>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -19,6 +19,8 @@
         include('./client/login.php');
     } elseif (isset($_GET['ask'])) {
         include('./client/ask.php');
+    } else {
+        include("./client/question.php");
     }
 
     ?>
