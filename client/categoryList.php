e71be3fdceed5a3f32f6e1493ce1f36eff19053e
Tushar <tusharpatel1093@gmail.com>
2025-06-25 11:04:35 +0530

    Category Listing

---
+++ client/categoryList.php
@@ -0,0 +1,14 @@
+<h1 class="heading">Category List</h1>
+<?php
+include("./common/db.php");
+$query = "select * from category";
+$result = $conn->query($query);
+foreach ($result as $row) {
+    $category = ucfirst($row['category']);
+    $id = $row['id'];
+    echo "<div class='row questionList'>
+        <h4><a href='?q-id=$id'>$category</a></h4>
+        </div>";
+}
+
+?>
--- client/question.php
+++ client/question.php
@@ -1,17 +1,24 @@
 <div class="container">
-    <h1 class="heading">Questions</h1>
-    <div class="col-8">
-        <?php
-        include("/Php/www/Discuss_Project/common/db.php");
-        $query = "select * from questions";
-        $result = $conn->query($query);
-        foreach ($result as $row) {
-            $title = $row['title'];
-            $id = $row['id'];
-            echo "<div class='row questionList'>
+    <div class="row">
+        <div class="col-8">
+            <h1 class="heading">Questions</h1>
+            <?php
+            include("/Php/www/Discuss_Project/common/db.php");
+            $query = "select * from questions";
+            $result = $conn->query($query);
+            foreach ($result as $row) {
+                $title = $row['title'];
+                $id = $row['id'];
+                echo "<div class='row questionList'>
         <h4><a href='?q-id=$id'>$title</a></h4>
         </div>";
-        }
-        ?>
+            }
+            ?>
+        </div>
+        <div class="col-4">
+            <?php
+            include("./client/categoryList.php");
+            ?>
+        </div>
     </div>
 </div>
